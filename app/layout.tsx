fe7e022044b65e60d3b27b7bc8b34f23d07b6f34
Ravi kumar <ravi@C-BL-WV-LT-0138.local>
2026-03-24 11:15:17 +0530

    fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,10 @@
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
+import Script from "next/script"
 import "./globals.css"
 import { SiteChrome } from "@/components/site-chrome"
+
+const FB_PIXEL_ID = "2146659069485723"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -34,6 +37,31 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '${FB_PIXEL_ID}');
+fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          {/* Facebook Pixel noscript fallback — must be a plain <img> */}
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src={`https://www.facebook.com/tr?id=${FB_PIXEL_ID}&ev=PageView&noscript=1`}
+            alt=""
+          />
+        </noscript>
         <SiteChrome>{children}</SiteChrome>
       </body>
     </html>
